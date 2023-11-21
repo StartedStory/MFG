b3e3659b042110636c67e10fb7cb8fa43b1362d3
Amr <topdev39@gmail.com>
2023-11-21 22:21:17 +0800

    Solor Control section

--- src/app/ToolsAndDownload/page.tsx
+++ src/app/ToolsAndDownload/page.tsx
@@ -1,11 +1,27 @@
 import Header from "../components/header";
 import Hero from "./hero";
+import SolarControl from "./solarControl";
+import ThermalInsulation from "./thermalInsulation";
+import Security from "./security";
+import ReflectiveLenses from "./reflectiveLenses";
+import TechnicalCatalog from "./technicalCatalog";
+import Bim from "./bim";
+import Certifications from "./certifications";
+import Footer from "../components/footer";
 
 const ToolsAndDownload = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <SolarControl />
+      <ThermalInsulation />
+      <Security />
+      <ReflectiveLenses />
+      <TechnicalCatalog />
+      <Bim />
+      <Certifications />
+      <Footer />
     </div>
   );
 };
---
+++ src/app/ToolsAndDownload/security.tsx
@@ -0,0 +1,61 @@
+const Security = () => {
+  return (
+    <div className="mt-20 mx-32">
+      <div className="flex flex-1 items-center">
+        <span className="flex flex-1 bg-toolsTitle w-auto h-[1px] border-t-1 border-toolsTitle"></span>
+        <h4 className="uppercase text-toolsLandingColor text-bold px-3">
+          Security
+        </h4>
+        <span className="flex flex-1 bg-toolsTitle w-auto h-[1px] border-t-1 border-toolsTitle"></span>
+      </div>
+      <div className="flex items-center">
+        <div className="mr-20 mt-12 w-1/4 h-T400">
+          <iframe
+            className="w-full h-3/4"
+            src={
+              "https://docs.google.com/viewer?url=http%3A%2F%2Fmfg.dz%2Fwp-content%2Fuploads%2F2017%2F10%2FD%C3%A9pliant_contr%C3%B4le-solaire_eng.pdf&embedded=true&hl=en"
+            }
+          ></iframe>
+          <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
+            Download
+          </button>
+        </div>
+        <div className="mr-20 mt-12 w-1/4 h-T400">
+          <iframe
+            className="w-full h-3/4"
+            src={
+              "https://docs.google.com/viewer?url=http%3A%2F%2Fmfg.dz%2Fwp-content%2Fuploads%2F2017%2F10%2FD%C3%A9pliant_contr%C3%B4le-solaire_eng.pdf&embedded=true&hl=en"
+            }
+          ></iframe>
+          <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
+            Download
+          </button>
+        </div>
+        <div className="mr-20 mt-12 w-1/4 h-T400">
+          <iframe
+            className="w-full h-3/4"
+            src={
+              "https://docs.google.com/viewer?url=http%3A%2F%2Fmfg.dz%2Fwp-content%2Fuploads%2F2017%2F10%2FD%C3%A9pliant_contr%C3%B4le-solaire_eng.pdf&embedded=true&hl=en"
+            }
+          ></iframe>
+          <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
+            Download
+          </button>
+        </div>
+        <div className="mr-20 mt-12 w-1/4 h-T400">
+          <iframe
+            className="w-full h-3/4"
+            src={
+              "https://docs.google.com/viewer?url=http%3A%2F%2Fmfg.dz%2Fwp-content%2Fuploads%2F2017%2F10%2FD%C3%A9pliant_contr%C3%B4le-solaire_eng.pdf&embedded=true&hl=en"
+            }
+          ></iframe>
+          <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
+            Download
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Security;
