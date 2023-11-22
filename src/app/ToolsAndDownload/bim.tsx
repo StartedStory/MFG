4dad89e3fe2934edac7886a921b8939638c193c2
Amr <topdev39@gmail.com>
2023-11-23 01:47:35 +0800

    Update the download section

--- src/app/ToolsAndDownload/bim.tsx
+++ src/app/ToolsAndDownload/bim.tsx
@@ -7,20 +7,32 @@ const Bim = () => {
         <span className="flex flex-1 bg-toolsTitle w-auto h-[1px] border-t-1 border-toolsTitle"></span>
       </div>
       <div className="flex items-center">
-        <div className="mr-48 mt-12 w-1/4 h-T400">
-          <iframe className="w-full h-3/4 bg-bim-background bg-contain"></iframe>
+        <div className="mx-auto border-b border-b-toolsLandingColor mt-12 pb-3 w-1/4 flex justify-around items-center">
+          <div className="text-sm font-Poppins text-productionColorTo">
+            Policy-QHSE-Fr-23.06.2020
+          </div>
           <a
             href="https://mfg.dz/wp-content/uploads/2018/03/MFG%20BIM.rar"
             download
           >
-            <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
-              Download
+            <button className="p-3 bg-toolsDownload text-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
             </button>
           </a>
         </div>
-        <div className="mr-20 mt-12 w-1/4 h-T400"></div>
-        <div className="mr-20 mt-12 w-1/4 h-T400"></div>
-        <div className="mr-20 mt-12 w-1/4 h-T400"></div>
       </div>
     </div>
   );
--- src/app/ToolsAndDownload/thermalInsulation.tsx
+++ src/app/ToolsAndDownload/thermalInsulation.tsx
@@ -9,83 +9,107 @@ const ThermalInsulation = () => {
         <span className="flex flex-1 bg-toolsTitle w-auto h-[1px] border-t-1 border-toolsTitle"></span>
       </div>
       <div className="flex items-center">
-        <div className="mr-20 mt-12 w-1/4 h-T400">
-          <iframe
-            className="w-full h-3/4"
-            src={
-              "https://docs.google.com/viewer?url=" +
-              encodeURIComponent(
-                "http://mfg.dz/wp-content/uploads/2020/09/Politique-QHSE-Fr-23.06.2020.pdf"
-              ) +
-              "&embedded=true&hl=en"
-            }
-          ></iframe>
+        <div className="mx-auto border-b border-b-toolsLandingColor mt-12 pb-3 w-1/4 flex justify-around items-center">
+          <div className="text-sm font-Poppins text-productionColorTo">
+            Policy-QHSE-Fr-23.06.2020
+          </div>
           <a
-            href="http://mfg.dz/wp-content/uploads/2017/09/D%C3%A9pliant-Isolation.pdf"
+            href="http://mfg.dz/wp-content/uploads/2017/10/D%C3%A9pliant_contr%C3%B4le-solaire_eng.pdf"
             download
           >
-            <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
-              Download
+            <button className="p-3 bg-toolsDownload text-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
             </button>
           </a>
         </div>
-        <div className="mr-20 mt-12 w-1/4 h-T400">
-          <iframe
-            className="w-full h-3/4"
-            src={
-              "https://docs.google.com/viewer?url=" +
-              encodeURIComponent(
-                "http://mfg.dz/wp-content/uploads/2020/09/Politique-QHSE-Fr-23.06.2020.pdf"
-              ) +
-              "&embedded=true&hl=en"
-            }
-          ></iframe>
+        <div className="mx-auto border-b border-b-toolsLandingColor mt-12 pb-3 w-1/4 flex justify-around items-center">
+          <div className="text-sm font-Poppins text-productionColorTo">
+            Policy-QHSE-Fr-23.06.2020
+          </div>
           <a
             href="http://mfg.dz/wp-content/uploads/2017/09/fiche_tech-MEDISTAR-S.pdf"
             download
           >
-            <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
-              Download
+            <button className="p-3 bg-toolsDownload text-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
             </button>
           </a>
         </div>
-        <div className="mr-20 mt-12 w-1/4 h-T400">
-          <iframe
-            className="w-full h-3/4"
-            src={
-              "https://docs.google.com/viewer?url=" +
-              encodeURIComponent(
-                "http://mfg.dz/wp-content/uploads/2020/09/Politique-QHSE-Fr-23.06.2020.pdf"
-              ) +
-              "&embedded=true&hl=en"
-            }
-          ></iframe>
+        <div className="mx-auto border-b border-b-toolsLandingColor mt-12 pb-3 w-1/4 flex justify-around items-center">
+          <div className="text-sm font-Poppins text-productionColorTo">
+            Policy-QHSE-Fr-23.06.2020
+          </div>
           <a
             href="http://mfg.dz/wp-content/uploads/2017/09/fiche_tech-MEDISTAR-S-T.pdf"
             download
           >
-            <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
-              Download
+            <button className="p-3 bg-toolsDownload text-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
             </button>
           </a>
         </div>
-        <div className="mr-20 mt-12 w-1/4 h-T400">
-          <iframe
-            className="w-full h-3/4"
-            src={
-              "https://docs.google.com/viewer?url=" +
-              encodeURIComponent(
-                "http://mfg.dz/wp-content/uploads/2020/09/Politique-QHSE-Fr-23.06.2020.pdf"
-              ) +
-              "&embedded=true&hl=en"
-            }
-          ></iframe>
+        <div className="mx-auto border-b border-b-toolsLandingColor mt-12 pb-3 w-1/4 flex justify-around items-center">
+          <div className="text-sm font-Poppins text-productionColorTo">
+            Policy-QHSE-Fr-23.06.2020
+          </div>
           <a
             href="http://mfg.dz/wp-content/uploads/2017/09/fiche_tech-MEDISTAR-S-1.pdf"
             download
           >
-            <button className="text-sm font-Poppins w-full px-24 py-3 mt-1 bg-toolsIframeButton">
-              Download
+            <button className="p-3 bg-toolsDownload text-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
             </button>
           </a>
         </div>
