ca487dcb7392a055cf27762777523bf97f9ef798
Amr <topdev39@gmail.com>
2023-11-28 17:00:49 +0800

    production section responsive

--- src/app/aboutUs/about.tsx
+++ src/app/aboutUs/about.tsx
@@ -43,7 +43,7 @@ const About = () => {
         className="grid bg-white px-7 xl:px-10 2xl:px-20 py-10 grid-rows-2 grid-cols-1 md:grid-cols-2 md:grid-rows-1 mx-auto"
       >
         {/* Text stuffs */}
-        <div className="flex flex-col space-y-4 container ml-5 md:ml-6 lg:ml-10">
+        <div className="flex flex-col space-y-4 container mt-16 md:mt-0 ml-5 md:ml-6 lg:ml-10">
           <div className="mt-2 flex justify-start items-end gap-3 text-secondary w-full">
             <hr className="mb-1 border-b-2 border-secondary w-24"></hr>
             <h2 className="uppercase font-Poppins font-bold">ABOUT US</h2>
@@ -52,7 +52,7 @@ const About = () => {
             <h3 className="text-left font-bold font-Montserrat text-3xl xl:text-5xl">
               Elevating glass innovations globally
             </h3>
-            <p className="text-gray-600 mt-5 xl:mt-10 text-sm xl:text-lg font-Poppins leading-8">
+            <p className="text-gray-600 mt-5 xl:mt-10 text-sm xl:text-lg font-Poppins leading-6 lg:leading-8">
               Mediterranean Float Glass (MFG SPA), a subsidiary of the CEVITAL
               group, was created in 2007 and is led by a team of young talents
               dedicated to progress and development. Today, MFG is one of
@@ -60,7 +60,7 @@ const About = () => {
               Blida, just 32 km from the port of Algiers, the head office and
               factory cover an area of ​​30 hectares.
             </p>
-            <p className="text-gray-600 mt-3 xl:mt-10 text-sm xl:text-lg font-Poppins leading-8">
+            <p className="text-gray-600 mt-3 xl:mt-10 text-sm xl:text-lg font-Poppins leading-6 lg:leading-8">
               In its first year of existence, MFG transformed Algeria from an
               exclusive importer of flat glass to an exporter, satisfying more
               than 90% of local demand. Beyond Algeria, MFG serves international
@@ -181,7 +181,7 @@ const About = () => {
         </div>
         {/* Image section */}
         <div className="relative p-auto md:p-6 md:pl-0 transform lg:w-full order-first flex-1">
-          <div id="images" className="w-full h-full justify-end">
+          <div id="images" className="w-full h-full justify-end ml-16 md:ml-0">
             <Image
               src="/assets/img/about2.png"
               alt="Building"
@@ -203,66 +203,12 @@ const About = () => {
                   alt="Building"
                   width={400}
                   height={400}
-                  className="absolute mx-28 mt-[14rem] md:mx-16 md:mt-[8rem] lg:mx-40 lg:my-40 w-80"
+                  className="absolute mx-28 mt-[14rem] md:mx-12 md:mt-[8rem] lg:mx-40 lg:my-40 w-80"
                 />
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="hidden md:flex lg:hidden">
-          <div className="swiper max-w-full  flex-col items-center justify-center relative ">
-            <div className="swiper-wrapper items-center flex-row my-4 ">
-              <div className="swiper-slide">
-                <div className="flex items-center gap-2  justify-center">
-                  <div className=" border border-blue-700 rounded-full p-1 w-20 h-20">
-                    <Image
-                      src="/assets/img/quality2.png"
-                      alt="Building"
-                      width={72}
-                      height={72}
-                      className="bg-primary rounded-full p-4"
-                    ></Image>
-                  </div>
-                  <span className="text-buttonColor font-Poppins text-2xl font-bold uppercase">
-                    QUALITY
-                  </span>
-                </div>
-              </div>
-              <div className="swiper-slide">
-                <div className="flex items-center gap-2  justify-center">
-                  <div className=" border border-blue-700 rounded-full flex p-1 w-20 h-20">
-                    <Image
-                      src="/assets/img/batiment.png"
-                      alt="Building"
-                      width={72}
-                      height={84}
-                      className="bg-primary rounded-full p-4"
-                    ></Image>
-                  </div>
-                  <span className="text-buttonColor font-Poppins text-2xl font-bold uppercase">
-                    BUILDING
-                  </span>
-                </div>
-              </div>
-              <div className="swiper-slide">
-                <div className="flex items-center gap-2  justify-center">
-                  <div className=" border border-blue-700 rounded-full p-1 w-20 h-20">
-                    <Image
-                      src="/assets/img/world.png"
-                      alt="Building"
-                      width={72}
-                      height={72}
-                      className="bg-primary rounded-full p-4"
-                    ></Image>
-                  </div>
-                  <span className="text-buttonColor font-Poppins text-2xl font-bold uppercase">
-                    EXPORT
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
--- src/app/aboutUs/production.tsx
+++ src/app/aboutUs/production.tsx
@@ -14,73 +14,147 @@ const Production = () => {
           </h2>
         </div>
         <div id="text" className="">
-          <h3 className="w-752 text-center justify-center font-Montserrat font-semibold text-5xl leading-productionLineHeight">
+          <h3 className="w-full text-center justify-center font-Montserrat font-semibold text-4xl lg:text-5xl leading-productionLineHeight">
             Rapid Growth and Expansion of Production
           </h3>
         </div>
       </div>
 
-      <div className="w-full h-32 mt-28 mb-16 flex justify-start items-start bg-gradient-to-r from-transparent to-transparent via-productionColorVia">
-        <div className="-mt-8 ml-32 pr-10">
+      <div className="w-full h-32 mt-28 mb-16 hidden sm:flex justify-start items-start bg-gradient-to-r from-transparent to-transparent via-productionColorVia">
+        <div className="-mt-2 md:-mt-5 lg:-mt-8 ml-32 pr-5 lg:pr-10">
           <Image
             alt="random"
             width={100}
             height={100}
-            className="w-auto"
+            className="w-1/2 lg:w-auto"
             src={"/assets/img/production/2009.svg"}
           ></Image>
-          <div className="uppercase mt-8 font-Poppins text-2xl font-semibold text-productionColorVia">
+          <div className="uppercase mt-5 lg:mt-8 font-Poppins text-xl lg:text-2xl font-semibold text-productionColorVia">
             2009
           </div>
-          <div className="font-Poppins text-lg leading-8">
+          <div className="font-Poppins text-sm lg:text-lg leading-6 lg:leading-8">
             Introduced a laminated glass line with a daily capacity of 235 tons,
             expanding the production of laminated glass.
           </div>
         </div>
-        <div className="-mt-8 pr-10">
+        <div className="-mt-2 md:-mt-5 lg:-mt-8 pr-5 lg:pr-10">
           <Image
             alt="random"
             width={100}
             height={100}
-            className="w-auto mr-auto"
+            className="w-1/2 lg:w-auto mr-auto"
             src={"/assets/img/production/2010.svg"}
           ></Image>
-          <div className="uppercase mt-8 font-Poppins text-2xl font-semibold text-productionColorVia">
+          <div className="uppercase mt-5 lg:mt-8 font-Poppins text-xl lg:text-2xl font-semibold text-productionColorVia">
             2010
           </div>
-          <div className="font-Poppins text-lg leading-8">
+          <div className="font-Poppins text-sm lg:text-lg leading-6 lg:leading-8">
             Introduced a laminated glass line with a daily capacity of 235 tons,
             expanding the production of laminated glass.
           </div>
         </div>
-        <div className="-mt-8 pr-10">
+        <div className="-mt-2 md:-mt-5 lg:-mt-8 pr-5 lg:pr-10">
           <Image
             alt="random"
             width={100}
             height={100}
-            className="w-auto mr-auto"
+            className="w-1/2 lg:w-auto mr-auto"
             src={"/assets/img/production/2011.svg"}
           ></Image>
-          <div className="uppercase mt-8 font-Poppins text-2xl font-semibold text-productionColorVia">
+          <div className="uppercase mt-5 lg:mt-8 font-Poppins text-xl lg:text-2xl font-semibold text-productionColorVia">
             2011
           </div>
-          <div className="font-Poppins text-lg leading-8">
+          <div className="font-Poppins text-sm lg:text-lg leading-6 lg:leading-8">
             Introduced a laminated glass line with a daily capacity of 235 tons,
             expanding the production of laminated glass.
           </div>
         </div>
-        <div className="-mt-8 pr-40">
+        <div className="-mt-2 md:-mt-5 lg:-mt-8 pr-24 lg:pr-40">
           <Image
             alt="random"
             width={100}
             height={100}
-            className="w-auto mr-auto"
+            className="w-1/2 lg:w-auto mr-auto"
             src={"/assets/img/production/2016.svg"}
           ></Image>
-          <div className="uppercase mt-8 font-Poppins text-2xl font-semibold text-productionColorVia">
+          <div className="uppercase mt-5 lg:mt-8 font-Poppins text-xl lg:text-2xl font-semibold text-productionColorVia">
             2016
           </div>
-          <div className="font-Poppins text-lg leading-8">
+          <div className="font-Poppins text-sm lg:text-lg leading-6 lg:leading-8">
+            Introduced a laminated glass line with a daily capacity of 235 tons,
+            expanding the production of laminated glass.
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute left-[72px] mr-auto w-10 h-[1000px] mt-96 flex sm:hidden bg-gradient-to-b from-transparent to-transparent via-productionColorVia z-10"></div>
+      <div className="w-full px-10 mt-32 flex sm:hidden items-start gap-5 z-20">
+        <Image
+          alt="random"
+          width={100}
+          height={100}
+          className="w-auto"
+          src={"/assets/img/production/2009.svg"}
+        ></Image>
+        <div>
+          <div className="uppercase font-Poppins text-2xl font-bold text-productionColorVia">
+            2009
+          </div>
+          <div className="font-Poppins text-lg leading-6">
+            Introduced a laminated glass line with a daily capacity of 235 tons,
+            expanding the production of laminated glass.
+          </div>
+        </div>
+      </div>
+      <div className="w-full px-10 mt-20 flex sm:hidden items-start gap-5 z-20">
+        <Image
+          alt="random"
+          width={100}
+          height={100}
+          className="w-auto"
+          src={"/assets/img/production/2010.svg"}
+        ></Image>
+        <div>
+          <div className="uppercase font-Poppins text-2xl font-bold text-productionColorVia">
+            2010
+          </div>
+          <div className="font-Poppins text-lg leading-6">
+            Introduced a laminated glass line with a daily capacity of 235 tons,
+            expanding the production of laminated glass.
+          </div>
+        </div>
+      </div>
+      <div className="w-full px-10 mt-20 flex sm:hidden items-start gap-5 z-20">
+        <Image
+          alt="random"
+          width={100}
+          height={100}
+          className="w-auto"
+          src={"/assets/img/production/2011.svg"}
+        ></Image>
+        <div>
+          <div className="uppercase font-Poppins text-2xl font-bold text-productionColorVia">
+            2011
+          </div>
+          <div className="font-Poppins text-lg leading-6">
+            Introduced a laminated glass line with a daily capacity of 235 tons,
+            expanding the production of laminated glass.
+          </div>
+        </div>
+      </div>
+      <div className="w-full px-10 mt-20 flex sm:hidden items-start gap-5 z-20">
+        <Image
+          alt="random"
+          width={100}
+          height={100}
+          className="w-auto"
+          src={"/assets/img/production/2016.svg"}
+        ></Image>
+        <div>
+          <div className="uppercase font-Poppins text-2xl font-bold text-productionColorVia">
+            2016
+          </div>
+          <div className="font-Poppins text-lg leading-6">
             Introduced a laminated glass line with a daily capacity of 235 tons,
             expanding the production of laminated glass.
           </div>
